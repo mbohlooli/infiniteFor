062074034abbfcf8397ac3e9384bc826900f7cc6
Mehrab bohlooli <bohlool82@gmail.com>
2022-08-31 12:21:20 +0430

    Fix scroll up jumping

--- src/app/infinite-scroll.directive.ts
+++ src/app/infinite-scroll.directive.ts
@@ -15,139 +15,100 @@ export class InfiniteScrollDirective implements AfterViewInit, OnChanges {
   totalPadding: number = 0;
   paddingBottom: number = 0;
   paddingTop: number = 0;
-  //TODO: change the default values to the value of initial scroll
   previousStartIndex = 0;
   previousEndIndex = Math.ceil(500 / this.rowHeight);
   map: Map<number, ViewRef> = new Map();
   loading: boolean = false;
   initialized: boolean = false;
 
-  constructor(private renderer: Renderer2) {
-    this.paddingBottom = this.totalPadding = (this.items.length - 3) * this.rowHeight;
-
-  }
+  constructor(private renderer: Renderer2) { }
 
   ngOnChanges(changes: SimpleChanges): void {
-    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
-    //Add '${implements OnChanges}' to the class.
-    // const viewportChanges = changes['viewport'].currentValue;
-    // if (viewportChanges != undefined) {
-    //   console.log('Inner ==> ', viewportChanges);
-    // const scrollTop = this.viewport.nativeElement.scrollTop;
     const scrollTop = 0;
     this.totalPadding = (this.items.length - 3) * this.rowHeight;
     this.paddingTop = scrollTop;
     this.paddingBottom = this.totalPadding - this.paddingTop;
-    // this.previousStartIndex = Math.floor(scrollTop / this.rowHeight);
-    // this.previousEndIndex = this.previousStartIndex + Math.ceil((500 + this.paddingTop - this.previousStartIndex * this.rowHeight) / this.rowHeight);
-    // this.renderer.setStyle(this.listHolder?.nativeElement, 'padding-top', `${this.paddingTop}px`);
-    // this.renderer.setStyle(this.listHolder?.nativeElement, 'padding-bottom', `${this.paddingBottom}px`);
-    // this.initialized = true;
-    // for (let i = this.previousStartIndex; i < this.previousEndIndex; i++) {
-    //   let view = this.listItem.createEmbeddedView({ index: i });
-    //   view.rootNodes[0].style.transform = `translate3d(0, ${(i - this.previousEndIndex) * this.rowHeight}px, 0)`;
-    //   this.scrollContainer.insert(view);
-    //   this.map.set(i, view);
-    // }
-    // }
   }
 
   ngAfterViewInit() {
     this.renderer.setStyle(this.listHolder?.nativeElement, 'padding-top', `${this.paddingTop}px`);
     this.renderer.setStyle(this.listHolder?.nativeElement, 'padding-bottom', `${this.paddingBottom}px`);
     for (let i = this.previousStartIndex; i <= this.previousEndIndex; i++) {
-      let view = this.listItem.createEmbeddedView({ index: i });
+      let view = this.getView(i) as EmbeddedViewRef<any>;
       view.rootNodes[0].style.transform = `translate3d(0, ${i * this.rowHeight}px, 0)`;
-      view.rootNodes[0].style.backgroundColor = `rgb(${12 * i}, ${7 * i}, ${10 * i})`;
+      view.rootNodes[0].style.backgroundColor = `rgb(${12 * i}, ${12 * i}, ${12 * i})`;
       this.scrollContainer.insert(view);
-      this.map.set(i, view);
+      view.reattach();
     }
   }
 
   @HostListener('scroll') onScroll() {
     const scrollTop = this.viewport?.nativeElement.scrollTop;
+    const scrollMax = this.viewport?.nativeElement.scrollMax;
     if (!scrollTop) return;
     this.paddingTop = scrollTop;
     this.paddingBottom = this.totalPadding - this.paddingTop;
+
     let startIndex = Math.floor(scrollTop / this.rowHeight);
-    let endIndex = startIndex + Math.ceil((500 + this.paddingTop - startIndex * this.rowHeight) / this.rowHeight);
+    let endIndex = 3 + startIndex + Math.ceil((500 + this.paddingTop - startIndex * this.rowHeight) / this.rowHeight);
 
 
-    console.log('scrolled');
-    console.log(startIndex, endIndex);
-    console.log('old positions', this.previousStartIndex, this.previousEndIndex);
-    console.log('scrollTop', scrollTop)
-
+    // console.log('scrolled');
+    // console.log(startIndex, endIndex);
+    // console.log('old positions', this.previousStartIndex, this.previousEndIndex);
+    // console.log('scrollTop', scrollTop)
     if (endIndex >= this.items.length && !this.loading) {
       console.log('loading more items...');
       this.loading = true;
       return;
     }
 
-    if (this.previousEndIndex < startIndex || this.previousStartIndex > endIndex) {
+    const fastScroll = this.previousEndIndex < startIndex || this.previousStartIndex > endIndex;
+    if (fastScroll) {
       for (let i = 0; i < this.scrollContainer.length; i++)
         this.scrollContainer.detach(i);
-      for (let i = startIndex; i <= endIndex; i++)
-        if (!this.map.has(i)) {
-          let view = this.listItem.createEmbeddedView({ index: i });
-          view.rootNodes[0].style.transform = `translate3d(0, ${(i - startIndex) * this.rowHeight - (scrollTop % this.rowHeight)}px, 0)`;
-          view.rootNodes[0].style.backgroundColor = `rgb(${12 * i}, ${7 * i}, ${10 * i})`;
-
-          this.scrollContainer.insert(view);
-          this.map.set(i, view);
-          view.reattach();
-        } else {
-          let view = this.map.get(i);
-          (view as EmbeddedViewRef<any>).rootNodes[0].style.transform = `translate3d(0, ${(i - startIndex) * this.rowHeight - (scrollTop % this.rowHeight)}px, 0)`;
-          if (view)
-            this.scrollContainer.insert(view);
-        }
+      for (let i = startIndex; i <= endIndex; i++) {
+        let view = this.getView(i) as EmbeddedViewRef<any>;
+        view.rootNodes[0].style.transform = `translate3d(0, ${(i - startIndex) * this.rowHeight - (scrollTop % this.rowHeight)}px, 0)`;
+        view.rootNodes[0].style.backgroundColor = `rgb(0, 0, ${12 * i})`;
+        this.scrollContainer.insert(view);
+        view.reattach();
+      }
     } else {
+      const scrollDown = this.previousStartIndex < startIndex;
+      const scrollUp = this.previousStartIndex > startIndex;
       //TODO: store the previous scroll
-      if (this.previousStartIndex < startIndex) {
-        console.log('=====CHECK1=====');
+      if (scrollDown) {
+        //! what if user scrolles to end and trigger this
+        //! then scroll top
+        //! then scroll down
+        //! booom!
+        if (this.loading) return;
         for (let i = this.previousStartIndex; i < startIndex; i++)
           this.scrollContainer.detach(i - this.previousStartIndex);
-        console.log('=====CHECK2=====');
         for (let i = startIndex; i < this.previousEndIndex; i++)
           (this.scrollContainer.get(i - startIndex) as EmbeddedViewRef<any>).rootNodes[0].style.transform =
             `translate3d(0, ${(i - startIndex) * this.rowHeight - (scrollTop % this.rowHeight)}px, 0)`;
         for (let i = this.previousEndIndex; i <= endIndex; i++) {
-          if (!this.map.has(i)) {
-            let view = this.listItem.createEmbeddedView({ index: i });
-            view.rootNodes[0].style.transform = `translate3d(0, ${(i - startIndex) * this.rowHeight - (scrollTop % this.rowHeight)}px, 0)`;
-            view.rootNodes[0].style.backgroundColor = `rgb(${12 * i}, ${7 * i}, ${10 * i})`;
-
-            this.scrollContainer.insert(view);
-            this.map.set(i, view);
-            view.reattach();
-          } else {
-            let view = this.map.get(i);
-            (view as EmbeddedViewRef<any>).rootNodes[0].style.transform = `translate3d(0, ${(i - startIndex) * this.rowHeight - (scrollTop % this.rowHeight)}px, 0)`;
-            if (view)
-              this.scrollContainer.insert(view);
-          }
+          let view = this.getView(i) as EmbeddedViewRef<any>;
+          view.rootNodes[0].style.transform = `translate3d(0, ${(i - startIndex) * this.rowHeight - (scrollTop % this.rowHeight)}px, 0)`;
+          view.rootNodes[0].style.backgroundColor = `rgb(${12 * i}, 0, 0)`;
+          this.scrollContainer.insert(view);
+          view.reattach();
         }
-      } else if (this.previousStartIndex > startIndex) {
-        for (let i = endIndex + 1; i < this.previousEndIndex; i++)
-          this.scrollContainer.detach(i - this.previousStartIndex);
+      } else if (scrollUp) {
+        if (this.loading) this.loading = false;
         for (let i = this.previousStartIndex; i <= endIndex; i++)
           (this.scrollContainer.get(i - this.previousStartIndex) as EmbeddedViewRef<any>).rootNodes[0].style.transform =
-            `translate3d(0, ${(i + startIndex - this.previousStartIndex) * this.rowHeight - (scrollTop % this.rowHeight)}px, 0)`;
+            `translate3d(0, ${(i - this.previousStartIndex) * this.rowHeight - (scrollTop % this.rowHeight)}px, 0)`;
+        for (let i = endIndex + 1; i < this.previousEndIndex; i++)
+          this.scrollContainer.detach(i - this.previousStartIndex);
         for (let i = this.previousStartIndex; i >= startIndex; i--) {
-          if (!this.map.has(i)) {
-            let view = this.listItem.createEmbeddedView({ index: i });
-            view.rootNodes[0].style.transform = `translate3d(0, ${(i - startIndex) * this.rowHeight - (scrollTop % this.rowHeight)}px, 0)`;
-            view.rootNodes[0].style['background-color'] = `rgb(10 * i, 15 *i, 15 *i)`;
-            this.scrollContainer.insert(view, 0);
-            this.map.set(i, view);
-            view.reattach();
-          } else {
-            let view = this.map.get(i);
-            (view as EmbeddedViewRef<any>).rootNodes[0].style.transform = `translate3d(0, ${(i - startIndex) * this.rowHeight - (scrollTop % this.rowHeight)}px, 0)`;
-            if (view)
-              this.scrollContainer.insert(view, 0);
-          }
+          let view = this.getView(i) as EmbeddedViewRef<any>;
+          view.rootNodes[0].style.transform = `translate3d(0, ${(i - startIndex) * this.rowHeight - (scrollTop % this.rowHeight)}px, 0)`;
+          view.rootNodes[0].style.backgroundColor = `rgb( 0,  ${12 * i},  0)`;
+          this.scrollContainer.insert(view, 0);
+          view.reattach();
         }
       } else {
         for (let i = 0; i < this.scrollContainer.length; i++)
@@ -161,5 +122,14 @@ export class InfiniteScrollDirective implements AfterViewInit, OnChanges {
     this.renderer.setStyle(this.listHolder.nativeElement, 'padding-bottom', `${this.paddingBottom}px`);
     this.previousStartIndex = startIndex;
     this.previousEndIndex = endIndex;
+  }
+
+  getView(index: number): ViewRef {
+    let view = this.map.get(index);
+    if (!view) {
+      view = this.listItem.createEmbeddedView({ index });
+      this.map.set(index, view);
+    }
+    return view;
   }
 }
